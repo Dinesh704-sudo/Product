19e47c676fe3d7d6ad98309a661fbb1f6a54cf97
ADMIN <ADMIN@ROHANPC>
2023-07-06 11:09:38 +0530

    Add project files.

---
+++ Product/Models/ProductSerivice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace Product.Models
+{
+    public class ProductSerivice
+    {
+        string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString.ToString();
+
+        public List<DeltawareProduct> GetProduct()
+        {
+            List<DeltawareProduct> dp = new List<DeltawareProduct>();
+            SqlConnection con = new SqlConnection(cs);
+            SqlCommand cmd = new SqlCommand("select * from DeltawareProduct", con);
+            cmd.CommandType = CommandType.Text;
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while(dr.Read())
+            {
+                DeltawareProduct dp1 = new DeltawareProduct();
+                dp1.ProductId = Convert.ToInt32(dr.GetValue(0).ToString());
+                dp1.Name = (dr.GetValue(1).ToString());
+                dp1.Description = (dr.GetValue(2).ToString());
+
+                dp.Add(dp1);
+            }
+
+            return dp;
+
+             
+
+        }
+    }
+     
+}
